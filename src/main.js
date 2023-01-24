c6f4ae8cdaf95d41e837ce9357d96a0454adcabe
Jos Rothman <jos@voxip.nl>
2023-01-24 09:06:15 +0100

    test

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import { Calendar } from '@fullcalendar/core';
-import timeGridPlugin from '@fullcalendar/timegrid';
+import { Calendar } from "@fullcalendar/react";
+import timeGridPlugin from "@fullcalendar/timegrid";
+
 let calendar = new Calendar(calendarEl, {
   plugins: [ timeGridPlugin ],
-  initialView: 'timeGridWeek'
+  initialView: 'timeGridDay'
 });
 
